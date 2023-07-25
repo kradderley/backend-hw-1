8ea1cc10c727dac65c8742b2e5d615dee1234106
Khes Adderley <khesadderley@khess-macbook-air.home>
2023-07-25 06:51:20 +0200

    some sql added

--- routes/get.js
+++ routes/get.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const asyncMySQL = require("../mysql/connection");
 
 // router.get("/tvshows", (req, res) => {
 //   res.send(req.series);
 // });
 
-router.get("/tvshow/:tv_id", (req, res) => {
-  // const tv_id = Number(req.params.tv_id);
+router.get("/tvshow/:tv_id", async (req, res) => {
+  const tv_id = Number(req.params.tv_id);
 
-  // if (Number.isNaN(tv_id) || tv_id < 1) {
-  //   res.send({ status: 0, reason: "Invalid entry" });
-  //   return;
-  // }
+  if (Number.isNaN(tv_id) || tv_id < 1) {
+    res.send({ status: 0, reason: "Invalid entry" });
+    return;
+  }
 
-  // const _series = [...req.series];
+  const results =
+    await asyncMySQL(`SELECT name, overview, media_type, first_air_date
+                                  FROM tv_show
+                                    WHERE id LIKE ${tv_id};`);
 
-  // const tvshow = _series.find((char) => {
-  //   return char.tv_id === tv_id;
-  // });
+  console.log(results);
+
+  if (results.length > 0) {
+    res.send({ status: 1 });
+    return;
+  }
 
   // if (!tvshow) {
   //   res.send({ status: 0, reason: "Tv show does not exist" });
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-// const series = require("./tvshow.json");
+const series = require("./tvshow.json");
 
-series.forEach((char, index) => {
-  char.tv_id = index + 1;
-});
+// series.forEach((char, index) => {
+//   char.tv_id = index + 1;
+// });
 
 app.use((req, res, next) => {
   req.series = series;
@@ -16,9 +16,9 @@ app.use(express.static("public"));
 app.use(express.json());
 
 app.use("/get", require("./routes/get"));
-app.use("/delete", require("./routes/delete"));
-app.use("/add", require("./routes/add"));
-app.use("/update", require("./routes/update"));
+// app.use("/delete", require("./routes/delete"));
+// app.use("/add", require("./routes/add"));
+// app.use("/update", require("./routes/update"));
 
 const port = process.env.PORT || 6001;
 app.listen(port, () => {
