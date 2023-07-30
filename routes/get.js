9cb9934ce0d48f6413b05fd9e3b5045eae80bd1b
Khes Adderley <khesadderley@khess-macbook-air.home>
2023-07-30 15:54:39 +0200

    updates made to routes

--- routes/get.js
+++ routes/get.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const asyncMySQL = require("../mysql/connection");
 
-// router.get("/tvshows", (req, res) => {
-//   res.send(req.series);
-// });
+router.get("/tvshows", (req, res) => {
+  res.send(req.series);
+});
 
-router.get("/tvshow/:tv_id", async (req, res) => {
+router.get("/tvshow/:tv_id", (req, res) => {
   const tv_id = Number(req.params.tv_id);
 
   if (Number.isNaN(tv_id) || tv_id < 1) {
@@ -14,23 +13,17 @@ router.get("/tvshow/:tv_id", async (req, res) => {
     return;
   }
 
-  const results =
-    await asyncMySQL(`SELECT name, overview, media_type, first_air_date
-                                  FROM tv_show
-                                    WHERE id LIKE ${tv_id};`);
+  const _series = [...req.series];
 
-  console.log(results);
+  const tvshow = _series.find((char) => {
+    return char.tv_id === tv_id;
+  });
 
-  if (results.length > 0) {
-    res.send({ status: 1 });
-    return;
+  if (!tvshow) {
+    res.send({ status: 0, reason: "Tv show does not exist" });
   }
 
-  // if (!tvshow) {
-  //   res.send({ status: 0, reason: "Tv show does not exist" });
-  // }
-
-  // res.send({ status: 1, tvshow });
+  res.send({ status: 1, tvshow });
 });
 
 module.exports = router;
--- routes/update.js
+++ routes/update.js
@@ -2,23 +2,48 @@ const express = require("express");
 const router = express.Router();
 
 router.patch("/tvshow/:tv_id", (req, res) => {
-    console.log(req.body);
-  // const tv_id = Number(req.params.tv_id);
+  const tv_id = Number(req.params.tv_id);
+  console.log(req.body, tv_id);
 
-  //   if (Number.isNaN(tv_id)) {
-  //     res.send({ status: 0, reason: "Invalid entry" });
-  //     return;
-  //   }
-  const name = req.params.name;
+  if (Number.isNaN(tv_id)) {
+    res.send({ status: 0, reason: "Invalid entry" });
+    return;
+  }
 
   const indexOf = req.series.findIndex((item) => {
-    return (item.name === name);
+    return item.tv_id === tv_id;
   });
 
-  req.series[indexOf] = { ...req.series[indexOf]}; 
+  if (indexOf < 0) {
+    res.send({ status: 0, reason: "Item does not exist" });
+    return;
+  }
+
+  const { adult, name, original_language, original_name, overview } = req.body;
+
+  if (adult && typeof adult === "boolean") {
+    req.series[indexOf].adult = adult;
+  }
+  if (name) {
+    req.series[indexOf].name = name;
+  }
+  if (overview && typeof overview === "string") {
+    req.series[indexOf].overview = overview;
+  }
+  if (original_name === name) {
+    req.series[indexOf].original_name = original_name;
+  }
+  if (
+    original_language &&
+    typeof original_language === "string" &&
+    original_language.length === 2
+  ) {
+    req.series[indexOf].original_language = original_language;
+  }
+
+  res.send({ status: 1, reason: "Update Successful" });
 
   console.log(indexOf);
-
 });
 
 module.exports = router;
