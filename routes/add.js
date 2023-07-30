6931c9c16342e3ada83ceea2a721fd101e7e3391
Khes Adderley <khesadderley@khess-macbook-air.home>
2023-07-30 13:49:33 +0200

    final updates made to update.js

--- routes/add.js
+++ routes/add.js
@@ -4,27 +4,29 @@ const router = express.Router();
 router.post("/tvshow", (req, res) => {
   const { adult, name, original_language, overview, media_type } = req.body;
 
+  // const tv_id = Number(req.params.tv_id) + 1;
+
   if (
     !adult ||
     !name ||
     !original_language ||
     !overview ||
-    !media_type ||
-    typeof adult !== "string" ||
-    typeof name !== "string" ||
-    typeof original_language !== "string" ||
-    typeof overview !== "string" ||
-    typeof media_type !== "string"
+    (!media_type &&
+      (typeof adult !== "boolean" ||
+        typeof name !== "string" ||
+        typeof original_language !== "string" ||
+        typeof overview !== "string" ||
+        typeof media_type !== "string"))
   ) {
     res.send({ status: 0, reason: "Not enough/incorrect information added" });
     return;
   }
 
   const indexOf = req.series.findIndex((item) => {
-    item.name === name;
+    return item.name === name || item.overview === overview;
   });
 
-  if (indexOf > -1) {
+  if (indexOf >= 0) {
     res.send({ status: 0, reason: "Tv show already added" });
     return;
   }
@@ -32,11 +34,11 @@ router.post("/tvshow", (req, res) => {
   //   how to add tv_id based on previous object ???
   req.series.push({
     adult,
-    id: Math.round(Math.random() * 100000),
     name,
     original_language,
     overview,
     media_type,
+    // tv_id: tv_id
   });
 
   res.send({ status: 1, reason: "New movie added" });
--- routes/update.js
+++ routes/update.js
@@ -2,23 +2,47 @@ const express = require("express");
 const router = express.Router();
 
 router.patch("/tvshow/:tv_id", (req, res) => {
-    console.log(req.body);
-  // const tv_id = Number(req.params.tv_id);
+  const tv_id = Number(req.params.tv_id);
+  const { adult, name, original_language, original_name, overview } = req.body;
+  console.log(req.body, tv_id);
 
-  //   if (Number.isNaN(tv_id)) {
-  //     res.send({ status: 0, reason: "Invalid entry" });
-  //     return;
-  //   }
-  const name = req.params.name;
+  if (Number.isNaN(tv_id)) {
+    res.send({ status: 0, reason: "Invalid entry" });
+    return;
+  }
 
   const indexOf = req.series.findIndex((item) => {
-    return (item.name === name);
+    return item.tv_id === tv_id;
   });
 
-  req.series[indexOf] = { ...req.series[indexOf]}; 
+  if (indexOf < 0) {
+    res.send({ status: 0, reason: "Item does not exist" });
+    return;
+  }
+
+  if (adult && typeof adult === "boolean") {
+    req.series[indexOf].adult = adult;
+  }
+  if (name) {
+    req.series[indexOf].name = name;
+  }
+  if (overview && typeof overview === "string") {
+    req.series[indexOf].overview = overview;
+  }
+  if (original_name === name) {
+    req.series[indexOf].original_name = original_name;
+  }
+  if (
+    original_language &&
+    typeof original_language === "string" &&
+    original_language.length === 2
+  ) {
+    req.series[indexOf].original_language = original_language;
+  }
+
+  res.send({ status: 1, reason: "Update Successful" });
 
   console.log(indexOf);
-
 });
 
 module.exports = router;
